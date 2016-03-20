2490a96c09f6ef5361f4bf4e6788cd702c3232c4
刘宇辉 <185127913@qq.com>
2016-03-20 23:19:46 +0800

    基本可用的配置文件，主题，补全，高亮，括号，powerline

---
+++ init.el
@@ -0,0 +1,44 @@
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-archives
+   (quote
+    (("gnu" . "http://elpa.gnu.org/packages/")
+     ("melpa" . "http://melpa.org/packages/"))))
+ '(package-selected-packages
+   (quote
+    (sicp shm flycheck-haskell flycheck rainbow-delimiters company-cabal company-web spaceline zenburn-theme xcscope syntax-subword swiper-helm solarized-theme projectile pretty-mode paredit mode-icons idle-highlight-mode helm-zhihu-daily helm-gtags haskell-snippets ggtags eyebrowse evalator emoji-cheat-sheet-plus emmet-mode electric-operator editorconfig company-math company-ghc auctex anaconda-mode)))
+ '(powerline-default-separator (quote wave))
+ '(scroll-bar-mode nil)
+ '(show-paren-mode t)
+ '(size-indication-mode t)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Source Code Pro" :foundry "outline" :slant normal :weight normal :width normal)))))
+
+
+(add-hook 'after-init-hook (lambda ()
+			     (load "~/.emacs.d/package-init.el")))
+;; highlight parenthesis
+(show-paren-mode 1)
+;; SubWord mode
+; use syntax-subword instead
+;(global-subword-mode 1)
+
+;; electric pair mode
+(electric-pair-mode 1)
+
+;; eldoc mode
+(global-eldoc-mode 1)
---
+++ package-init.el
@@ -0,0 +1,81 @@
+
+;; company mode backends
+(global-company-mode)
+(add-to-list 'company-backends 'company-ghc)
+(add-to-list 'company-backends 'company-math-symbols-unicode)
+
+;; smex
+;(require 'smex)
+;(smex-initialize)
+;
+;(global-set-key (kbd "M-x") 'smex)
+;(global-set-key (kbd "M-x") 'smex-major-mode-commands)
+;(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)))
+
+;; helm
+(require 'helm-config)
+; starts helm at startup
+(helm-mode 1)
+;; bind helm-M-x to M-x
+(global-set-key (kbd "M-x") 'helm-M-x)
+;; bind helm-find-files to C-x C-f
+;; and rebound origin find-file to C-x c C-x C-f
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-x c C-x C-f") 'find-file)
+
+;; bind helm-buffers-list to C-x C-b
+; C-x b suffice
+;(global-set-key (kbd "C-x C-b") 'helm-buffers-list)
+;(global-set-key (kbd "C-x c C-x C-b") 'buffer-list)
+
+;; zenburn theme
+(load-theme 'zenburn t)
+
+;; powerline
+(powerline-default-theme)
+
+;; haskell-mode-hook
+(defun my-haskell-setup ()
+  (haskell-doc-mode)
+  (haskell-indentation-mode)
+  (interactive-haskell-mode +1)
+  (setq-local company-backends
+	      (append '((company-ghc company-cabal))
+		      company-backends)))
+  
+(add-hook 'haskell-mode-hook 'my-haskell-setup)
+
+;; AucTeX mode company-math
+(defun my-TeX-setup ()
+  (setq-local company-backends
+	      (append '((company-math-symbols-latex company-latex-commands))
+		      company-backends)))
+
+(add-hook 'TeX-mode-hook 'my-TeX-setup)
+
+;; idle-highlight-mode
+(idle-highlight-mode 1)
+
+;; pretty-mode
+(global-pretty-mode 1)
+
+;; syntax-subword-mode
+(global-syntax-subword-mode 1)
+
+;; emmet
+(defun my-html-setup ()
+  (emmet-mode t)
+  (setq-local company-backends
+	      (append '((company-web-html))
+		      company-backends)))
+
+(add-hook 'html-mode-hook 'my-html-setup)
+
+;; lisp mode: electric-pair-mode, paredit-mode
+(defun my-lisp-setup ()
+  (enable-paredit-mode))
+
+(add-hook 'lisp-mode-hook 'my-lisp-setup)
+
+;; rainbow-delimiters-mode
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
