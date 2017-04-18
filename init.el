433eaae52e55280cc6aa385fad85e9dc815c3243
刘宇辉 <185127913@qq.com>
2017-04-18 15:08:46 +0800

    调整字体大小到12

--- init.el
+++ init.el
@@ -26,7 +26,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Source Code Pro" :foundry "outline" :slant normal :weight normal :width normal :height 98)))))
+ '(default ((t (:family "Source Code Pro" :foundry "ADBO" :slant normal :weight normal :width normal :height 120)))))
 
 
 (add-hook 'after-init-hook (lambda ()
